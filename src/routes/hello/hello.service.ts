04e66c031d317ca4142052e25615710a05b50648
hieudoanm <hieumdoan@gmail.com>
2022-07-22 15:51:05 +0700

    initial

---
+++ src/routes/hello/hello.service.ts
@@ -0,0 +1,17 @@
+import { HelloWorldResponse } from './hello.types';
+
+export const get = (): HelloWorldResponse => {
+  return { hello: 'world' };
+};
+
+export const create = (): HelloWorldResponse => {
+  return { hello: 'world' };
+};
+
+export const update = (): HelloWorldResponse => {
+  return { hello: 'world' };
+};
+
+export const remove = (): HelloWorldResponse => {
+  return { hello: 'world' };
+};
