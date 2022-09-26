09b25e22dd58e57a88269aed40d5d1d758933651
hieudoanm <hieumdoan@gmail.com>
2022-09-26 09:56:05 +0700

    Set up Redis Pub/Sub

---
+++ messages/redis-starter/consumer/src/app.ts
@@ -0,0 +1,25 @@
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import express, { json, urlencoded } from 'express';
+import helmet from 'helmet';
+import { RegisterRoutes } from './routes';
+import { errorHandler } from './middlewares/error';
+import { notFoundHandler } from './middlewares/not-found';
+
+const app = express();
+app.use(json());
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(cookieParser());
+app.use(morgan('tiny'));
+app.use(urlencoded({ extended: true }));
+
+RegisterRoutes(app);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+export default app;
